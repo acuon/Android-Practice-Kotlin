306d2e42b44bd76cf72aed1cf521da0e7306abe8
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-08-28 17:49:11 +0530

    Update MainActivity.kt

--- Fragments/Events/app/src/main/java/com/example/events/MainActivity.kt
+++ Fragments/Events/app/src/main/java/com/example/events/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.events
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
