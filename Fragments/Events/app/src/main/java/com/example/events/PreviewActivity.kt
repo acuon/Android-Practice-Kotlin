198b1d987b0708d7a1038424f1b4e56bcf644d5a
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-11-10 09:50:17 +0530

    Update PreviewActivity.kt

--- Fragments/Events/app/src/main/java/com/example/events/PreviewActivity.kt
+++ Fragments/Events/app/src/main/java/com/example/events/PreviewActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_preview.*
 
 class PreviewActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
